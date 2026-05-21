622dbd3453dc9dfa1e0b2ff2e6041a2baf435a5e
diorestu <diorestusaputra@gmail.com>
2026-05-21 16:10:43 +0800

    add GA4 and Meta Pixel

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -70,23 +70,39 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap" rel="stylesheet">
 
-        @if (in_array($page['component'] ?? null, ['welcome', 'landing/workable'], true))
-            <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WNXD5DZY5C"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-05GPX3DNS1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-                gtag('config', 'G-WNXD5DZY5C');
-            </script>
-        @endif
+            gtag('config', 'G-05GPX3DNS1');
+        </script>
+
+        <!-- Meta Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1331584195833501');
+            fbq('track', 'PageView');
+        </script>
+        <!-- End Meta Pixel Code -->
 
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
+        <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1331584195833501&ev=PageView&noscript=1"
+        /></noscript>
         @inertia
     </body>
 </html>
