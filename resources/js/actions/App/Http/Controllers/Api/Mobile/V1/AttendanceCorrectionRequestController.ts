f60bf81b51b88a0e1ab56ca7df16ee563f9018be
diorestu <diorestusaputra@gmail.com>
2026-07-29 11:40:22 +0800

    chore: regenerate attendance request routes

--- resources/js/actions/App/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.ts
+++ resources/js/actions/App/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.ts
@@ -1,7 +1,7 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::index
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:19
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:22
 * @route '/portal/api/attendance-requests'
 */
 export const index = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
@@ -16,7 +16,7 @@ index.definition = {
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::index
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:19
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:22
 * @route '/portal/api/attendance-requests'
 */
 index.url = (options?: RouteQueryOptions) => {
@@ -25,7 +25,7 @@ index.url = (options?: RouteQueryOptions) => {
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::index
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:19
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:22
 * @route '/portal/api/attendance-requests'
 */
 index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
@@ -35,7 +35,7 @@ index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::index
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:19
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:22
 * @route '/portal/api/attendance-requests'
 */
 index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
@@ -45,7 +45,7 @@ index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::index
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:19
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:22
 * @route '/portal/api/attendance-requests'
 */
 const indexForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
@@ -55,7 +55,7 @@ const indexForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => (
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::index
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:19
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:22
 * @route '/portal/api/attendance-requests'
 */
 indexForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
@@ -65,7 +65,7 @@ indexForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::index
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:19
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:22
 * @route '/portal/api/attendance-requests'
 */
 indexForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
@@ -82,7 +82,7 @@ index.form = indexForm
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::store
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:40
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:44
 * @route '/portal/api/attendance-requests'
 */
 export const store = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
@@ -97,7 +97,7 @@ store.definition = {
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::store
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:40
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:44
 * @route '/portal/api/attendance-requests'
 */
 store.url = (options?: RouteQueryOptions) => {
@@ -106,7 +106,7 @@ store.url = (options?: RouteQueryOptions) => {
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::store
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:40
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:44
 * @route '/portal/api/attendance-requests'
 */
 store.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
@@ -116,7 +116,7 @@ store.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::store
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:40
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:44
 * @route '/portal/api/attendance-requests'
 */
 const storeForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
@@ -126,7 +126,7 @@ const storeForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => 
 
 /**
 * @see \App\Http\Controllers\Api\Mobile\V1\AttendanceCorrectionRequestController::store
-* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:40
+* @see app/Http/Controllers/Api/Mobile/V1/AttendanceCorrectionRequestController.php:44
 * @route '/portal/api/attendance-requests'
 */
 storeForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
